bbfcf96af52df4548320c480f3159ec59660db13
david <david.c.aq@gmail.com>
2015-10-17 00:15:45 +0000

    修改org-mode升级到8.2后base.js的兼容问题

--- js/base.js
+++ js/base.js
@@ -2,13 +2,16 @@ $(function(){
     //title
     var title = $("h1.title").text();
     $("h1.title").remove();
+    if($("#preamble").length == 0){
+	$("body").prepend("<div id='preamble'></div>");
+    }
     $("#preamble").append("<div class=\"container\"><h1>"+title+"</h1></div>").addClass("bs-header");
 
     //布局
     $("#content").addClass("container bs-docs-container");
     $("#table-of-contents").addClass("col-md-3");
     $("#table-of-contents > h2").remove();
-    
+
     $(".outline-2").wrapAll("<div id=\"outline-2\" class=\"col-md-9\" role=\"main\"></div>");
     $("#table-of-contents,#outline-2").wrapAll("<div class=\"row\"></div>");
     //布局end
